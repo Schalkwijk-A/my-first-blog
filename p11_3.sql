d9b073e896dbd37780980f82834ef42cc3c79c4e
Alan <alanschalkwijk@ezweb.ne.jp>
2018-07-09 16:23:52 +0900

    Modified templates to display posts from database.

---
+++ p11_3.sql
@@ -0,0 +1,6 @@
+.mode column
+.headers on
+SELECT title,name FROM bookCreator
+INNER JOIN creator ON bookCreator.creatorId=creator.id
+INNER JOIN book ON bookCreator.bookId=book.id
+WHERE bookId = 4
